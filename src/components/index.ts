3d201b9bce06ce50c4509d14d97b1ea539fb53d7
barry <onlylonger@outlook.com>
2025-12-20 00:29:02 +0800

    init

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,4 @@ export { HeadlessToggle } from './HeadlessToggle'
 export { HeadlessCounter } from './HeadlessCounter'
 export type { HeadlessToggleProps, HeadlessCounterProps } from './types'
 
-
-
 console.log('Hello, world!')
